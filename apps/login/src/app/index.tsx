0b210c809eecf309af53657ff9c2b04feee11a7a
Bruno Garcia Moura <itsme@bgarciamoura.com>
2024-09-17 18:45:18 -0300

    chore: Removed some unused vars

--- apps/login/src/app/index.tsx
+++ apps/login/src/app/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
 
 export const Login = () => {
 
